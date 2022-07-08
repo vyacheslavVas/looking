7495e74c59e3a9b332dd5251d826a8a0228ff413
Vyacheslav Vasilyev <288s@mail.ru>
2022-07-08 15:54:01 +0300

    removed clear form

--- main.js
+++ main.js
@@ -5,11 +5,7 @@ $(function(){
     let closePopup = function(){
         $('.popup-container').css('display','none')
     };
-    let clearForm = function(){
-        $('.popup-container > input[type="text"]').val('');
-    };
     $('.close').on('click',function(){
-        clearForm();
         closePopup();
     });    
 });
