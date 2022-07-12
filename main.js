e1b6e26734671e6da62820f4a04d7394c6e086bc
Vyacheslav Vasilyev <288s@mail.ru>
2022-07-12 10:38:50 +0300

    2 changes back

--- main.js
+++ main.js
@@ -5,7 +5,11 @@ $(function(){
     let closePopup = function(){
         $('.popup-container').css('display','none')
     };
+    let clearForm = function(){
+        $('.popup-container > input[type="text"]').val('');
+    };
     $('.close').on('click',function(){
+        clearForm();
         closePopup();
     });    
 });
